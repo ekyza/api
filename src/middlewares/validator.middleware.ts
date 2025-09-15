b796ebc4f3c71836cd9c66ed460f3ba4621f49a2
ekyza <ekyza.dev@gmail.com>
2025-09-15 08:08:35 +0700

    add middleware to handle token validation

--- src/middlewares/validator.middleware.ts
+++ src/middlewares/validator.middleware.ts
@@ -1,9 +1,10 @@
 import { Request, Response, NextFunction } from "express";
-import { ZodSchema } from "zod";
+import jwt, { JwtPayload } from "jsonwebtoken";
+import { ZodType } from "zod";
 
 import { HttpStatus } from "../utils/httpStatus";
 
-export const validateRequest = (schema: ZodSchema, target: "body" | "query" | "params" = "body") => {
+export const validateRequest = (schema: ZodType, target: "body" | "query" | "params" = "body") => {
   return (req: Request, res: Response, next: NextFunction) => {
     const result = schema.safeParse(req[target]);
 
@@ -22,3 +23,44 @@ export const validateRequest = (schema: ZodSchema, target: "body" | "query" | "p
     next();
   };
 };
+
+export async function validateToken(req: Request, res: Response, next: NextFunction) {
+  const authorizationToken = req.headers.authorization;
+
+  if (!authorizationToken) {
+    res.status(HttpStatus.UNAUTHORIZED).json({
+      message: "Unauthorized. Token is required",
+    });
+
+    return;
+  }
+
+  if (!process.env.SECRET_JWT_TOKEN) {
+    res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+      message: "Internal server error: Missing JWT token secret.",
+    });
+
+    return;
+  }
+
+  try {
+    const token = authorizationToken.split(" ").at(1);
+
+    if (!token) {
+      res.status(HttpStatus.FORBIDDEN).json({
+        message: "Token is invalid or has expired.",
+      });
+
+      return;
+    }
+
+    const { id } = jwt.verify(token, process.env.SECRET_JWT_TOKEN) as JwtPayload;
+    res.locals.tokenPayload = { id };
+
+    next();
+  } catch (error) {
+    res.status(HttpStatus.FORBIDDEN).json({
+      message: "Token is invalid or has expired.",
+    });
+  }
+}
